dc78362dffacbcb9d83c00f0273379f7844e1373
Kevin Bolding <bolding@neuro.duke.edu>
2014-11-12 12:51:35 -0500

    Major Overhaul of Klusta Preprocessing to handle large files.

--- KlustaPrePro/AbValReferee.m
+++ KlustaPrePro/AbValReferee.m
@@ -1,45 +1,70 @@
-function [hldr] = AbValReferee(data)
+function [hldr1,hldr2] = AbValReferee(data,ChannelCount,Bloc)
+%% make a data matrix
+if(isempty(Bloc))
+    data = reshape(data,[ChannelCount length(data)/ChannelCount]); % to get back to linearized just use (:) on a matrix this shape
+    data = double(data);
+else
+    for j=0:(length(data)/ChannelCount)-1 %Split 48 or 64 channel data into two separate matrices
+        data1((1+j*(Bloc-1)):(Bloc-1+j*(Bloc-1)))=data((1+j*ChannelCount):(Bloc-1+j*ChannelCount));
+        data2((1+j*(ChannelCount-Bloc+1)):((ChannelCount-Bloc+1)+j*(ChannelCount-Bloc+1)))=data((Bloc+j*ChannelCount):(ChannelCount+j*ChannelCount));
+    end
+    data1=data1';
+    data2=data2';
+    data1 = reshape(data1,[(Bloc-1) length(data1)/(Bloc-1)]);
+    data1 = double(data1);
+    data2 = reshape(data2,[(ChannelCount-Bloc+1) length(data2)/(ChannelCount-Bloc+1)]);
+    data2 = double(data2);
+end
 
-%% make a data matrix 
-data = reshape(data,[32 length(data)/32]); % to get back to linearized just use (:) on a matrix this shape
-data = double(data);
 
-%% cut line noise
-[B,A] = butter(2,[59/15000 61/15000],'stop');
-dataline = filtfilt(B,A,data');
-dataline = dataline';
-
-%% high pass filter the data
-low = 100;
-Fs = 30000;
-[D,C] = butter(2,low/(Fs/2),'high');
-data = filtfilt(D,C,dataline');
-data = data';
-
-%% Exclude weird or dead channels from consideration as references.
-% channels whose variance is different from the average variance by 1 SD
-% are bad
-a = std(data');
-a = a';
-b = abs((a-mean(a))/std(a));
-
-Excludos = b>1;
-
-%% Find the absolute value of the data.
-abdata = abs(data);
-abdata(Excludos,:) = NaN;  % get rid of bad channels
-
-%% now sort the magnitude of abdata matrix
-[y,idx] = sort(abdata);
-
-%% average some number of channels with low absolute values
-chavnumb = 6;
-cnidx = sub2ind(size(abdata),idx(1:chavnumb,:),repmat(1:size(abdata,2),chavnumb,1));
-refch = mean(data(cnidx),1);
-  
-%% now use the raw data for subtraction.
-% and subtract it from the original data
-datarefd = bsxfun(@minus,data,refch);
-
-%% writing a headerless dat file for klustakwik
-hldr = int16(datarefd(:));
+for count=1:2
+    if(count==2 & ~exist('data2')) hldr2=[]; break; end
+    if(count==1 & exist('data1')) data=data1; end
+    if(count==2 & exist('data2')) data=data2; end
+    %% cut line noise
+    [B,A] = butter(2,[59/15000 61/15000],'stop');
+    dataline = filtfilt(B,A,data');
+    dataline = dataline';
+    
+    %% high pass filter the data
+    low = 100;
+    Fs = 30000;
+    [D,C] = butter(2,low/(Fs/2),'high');
+    data = filtfilt(D,C,dataline');
+    data = data';
+    
+    
+    
+    %% Exclude weird or dead channels from consideration as references.
+    % channels whose variance is different from the average variance by 1 SD
+    % are bad
+    a = std(data');
+    a = a';
+    b = abs((a-mean(a))/std(a));
+    
+    Excludos = b>1;
+    
+    %% Find the absolute value of the data.
+    abdata = abs(data);
+    abdata(Excludos,:) = NaN;  % get rid of bad channels
+    
+    %% now sort the magnitude of abdata matrix
+    [y,idx] = sort(abdata);
+    
+    %% average some number of channels with low absolute values
+    chavnumb = 6;
+    cnidx = sub2ind(size(abdata),idx(1:chavnumb,:),repmat(1:size(abdata,2),chavnumb,1));
+    refch = mean(data(cnidx),1);
+    
+    %% now use the raw data for subtraction.
+    % and subtract it from the original data
+    datarefd = bsxfun(@minus,data,refch);
+    
+    %% writing a headerless dat file for klustakwik
+    if count==1
+        hldr1 = int16(datarefd(:));
+    else
+        hldr2=int16(datarefd(:));
+    end
+end
+end
--- KlustaPrePro/NS6Concatenator.m
+++ KlustaPrePro/NS6Concatenator.m
@@ -2,40 +2,127 @@ clear all
 close all
 clc
 
-%% Hey, Kevin. You probably have to concatenate the NS3 file as well so you 
+%% Hey, Kevin. You probably have to concatenate the NS3 file as well so you
 % can match experimental events with these spikes. That means you have to
 % think about the header.
 
 
 %% Select all files to concatenate
- [fname, path] = uigetfile('*.ns*', 'Choose an NSx file...','MultiSelect','on');
- if ~iscell(fname)
- fname = {fname};
- end
- 
-%% Collect headerless versions of data into datas cell
-    
-for i = 1:length(fname)    
-    %% open data files sequentially headerlessly
-    datas{i} = openNSxHL([path fname{i}]);
-%     %% for testing. truncate
-%     datas{i} = datas{i}(1:180*32*30000);
-    %% Do referencing via SchooFi absolute value maneuver
-    datas{i} = AbValReferee(datas{i});
+[fname, path] = uigetfile('*.ns*', 'Choose an NSx file...','MultiSelect','on');
+if ~iscell(fname)
+    fname = {fname};
 end
 
+% fname is a cell containing all filenames to concatenate.
+% make chunks for each file separately
+for k = 1:length(fname)
+    FID = fopen([path fname{k}], 'r', 'ieee-le'); % open a file
+    dataHeaderBytes = 9; % headerbytes number from NPMK open* scripts
+    fseek(FID, 8, 'bof'); % move past filetype specifying bits
+    BasicHeader   = fread(FID, 306, '*uint8'); % read in the basic header to get HeaderBytes and ChannelCount
+    ChannelCount = double(typecast(BasicHeader(303:306), 'uint32')); % pull ChannelCount out of the header
+    %% find the connector bank letters to determine which channel
+    %  switches to a new probe (if the data is from multiple probes)
+    readSize= double(ChannelCount * 66);
+    ExtendedHeader = fread(FID, readSize, '*uint8'); % connector bank info is in here
+    for headerIDX = 1:ChannelCount %gathering all the A's and B's
+		offset = double((headerIDX-1)*66);
+        BankNum(headerIDX)=char(ExtendedHeader(21+offset) + ('A' - 1));
+    end
+    Bloc=find(BankNum=='B');
+    if numel(Bloc)>0 Bloc=Bloc(1); end
+    
+    %%
+    HeaderBytes   = double(typecast(BasicHeader(3:6), 'uint32')) + dataHeaderBytes; % how many Bytes to skip before data
+    fseek(FID, 0, 'eof'); % move to the end of the file
+    DataPoints = double(ftell(FID))-HeaderBytes; % measure how long the data is
+    
+    
+    % work in 10 minute chunks, this should be set to the max multiple that Memory can handle
+    ChunkSize = 32*600*30000; 
+    
+    % create a chunks folder if it doesn't exist, in the
+    % UnitSortingAnalysis folder
+    if ~exist([path,'chunks'],'dir')
+        mkdir(path,'chunks');
+    end
+    
+    % Start from headerbytes+0 and read a chunk (eg 0:1). next time start from one
+    % chunk size later (1:2). last iteration read to end of file (eg 2:2.5)
+    for j = 0:floor(DataPoints/ChunkSize)
+        ChunkPosition = HeaderBytes + (j)*ChunkSize;
+        fseek(FID,ChunkPosition, 'bof');
+        if j == floor(DataPoints/ChunkSize) % indicating last iteration
+            Chunk = fread(FID, inf, '*int16');
+        else
+            Chunk = fread(FID, ChunkSize, '*int16'); % We still don't know why, but ChunkSize has to be divided by 2
+        end
+        
+        % Do the filtering and referencing for this chunk.
+        [ChunkAVR1 ChunkAVR2] = AbValReferee(Chunk,ChannelCount,Bloc); 
+        size(ChunkAVR1)
+        size(ChunkAVR2)
+        % Chunk naming convention .chunk1.1 1.2 2.1 etc
+        newfname = [path 'chunks\' fname{k}(1:15) '.chunk' num2str(k) '.' num2str(j)];
+        % Opening the output file for saving
+        FIDw = fopen(newfname, 'w+', 'ieee-le');
+        fwrite(FIDw, ChunkAVR, 'int16');
+        fclose(FIDw);
+    end
+    
+    fclose(FID);
+    ChunksPerFile(k) = j+1; 
+end
+%%
+CatSeries = [];
+%
+for k = 1:length(fname)
+    D = dir([path,'chunks\*.chunk',num2str(k),'*']);
+    [~,order] = sort( {D.name} );
+    D = D(order);
+    CatList = {D.name};
+    for j = 1:ChunksPerFile(k)
+        CatSeries = [CatSeries CatList{j} ,'+'];        
+    end
+end
+CatCmd = ['copy /b ' CatSeries(1:end-1) ,' ', fname{1}(1:12) 'cat.dat'];
 
 
-%% Concatenate all datas.
-data = cell2mat(datas');
+cd([path,'chunks']);% move to the chunks directory
+system(CatCmd);
+system('del *chunk*')
+cd c:;
 
-%% Choose a name for your new giant file
-newfname = [path fname{1}(1:15),'-Line-HP100-Ref.dat'];
+%%
+%   %% in cmd.exe run this:
+%   copy /b Z:\UnitSortingAnalysis\31-Jul-2014_Analysis\chunks\*.* z:\UnitSortingAnalysis\chunked.dat
+% system('copy /b Z:\UnitSortingAnalysis\31-Jul-2014_Analysis\chunks\*.* z:\UnitSortingAnalysis\chunked1.dat')
 
-% Opening the output file for saving
-FIDw = fopen(newfname, 'w+', 'ieee-le');
 
-% Writing data into file
-disp('Writing the converted data into the new .dat file...');
-fwrite(FIDw, data, 'int16');
-fclose(FIDw);
+
+% %% Collect headerless versions of data into datas cell
+%
+% for i = 1:length(fname)
+%     %% open data files sequentially headerlessly
+%     datas{i} = openNSxHL([path fname{i}]);
+% %     %% for testing. truncate
+% %     datas{i} = datas{i}(1:180*32*30000);
+%     %% Do referencing via SchooFi absolute value maneuver
+%     datas{i} = AbValReferee(datas{i});
+% end
+%
+%
+%
+% %% Concatenate all datas.
+% data = cell2mat(datas');
+%
+% %% Choose a name for your new giant file
+% newfname = [path fname{1}(1:15),'-Line-HP100-Ref.dat'];
+%
+% % Opening the output file for saving
+% FIDw = fopen(newfname, 'w+', 'ieee-le');
+%
+% % Writing data into file
+% disp('Writing the converted data into the new .dat file...');
+% fwrite(FIDw, data, 'int16');
+% fclose(FIDw);
