0efe32e637f8ad81a1acc8978b0291f39be23465
Kevin Bolding <bolding@neuro.duke.edu>
2014-11-17 16:23:11 -0500

    More multiple shanks

--- PARIS/UnitLevel/GatherInfo1/CreateSpikeTimes/SpikeTimesKK.m
+++ PARIS/UnitLevel/GatherInfo1/CreateSpikeTimes/SpikeTimesKK.m
@@ -2,32 +2,45 @@ function [UnitID] = SpikeTimesKK(FilesKK)
 % SpikeTimesKK will return a structure called UnitID with tsec, chans, and
 % units. UnitID{1} is the sum of all sorted Units.
 
-spiketimes = double(hdf5read(FilesKK.KWIK, '/channel_groups/0/spikes/time_samples'));
-clusternumbers = double(hdf5read(FilesKK.KWIK, '/channel_groups/0/spikes/clusters/main'));
-allwaveforms = hdf5read(FilesKK.KWX, '/channel_groups/0/waveforms_raw');
+%Checks if there are multiple probes
+try
+    h5readatt(FilesKK.KWIK,'/channel_groups/1','name');
+catch err
+        probe='0'; 
+end
+ if(~exist('err'))
+     probe='0';
+        %probe=num2str(input('There are multiple probes. Which would you like to analyze? (0 or 1): '));
+ end
+%%
+spiketimes = double(hdf5read(FilesKK.KWIK, ['/channel_groups/',probe,'/spikes/time_samples']));
+clusternumbers = double(hdf5read(FilesKK.KWIK, ['/channel_groups/',probe,'/spikes/clusters/main']));
+allwaveforms = hdf5read(FilesKK.KWX, ['/channel_groups/',probe,'/waveforms_raw']);
 
 unitlist = unique(clusternumbers);
 
 for count=1:length(unitlist)
 
-    str=['/channel_groups/0/clusters/main/',num2str(unitlist(count))];
+    str=['/channel_groups/',probe,'/clusters/main/',num2str(unitlist(count))];
     clustergroups(count) = double(h5readatt(FilesKK.KWIK,str,'cluster_group'));
 end
 
 % For only sorted MUA
 GoodClusters=unitlist(clustergroups==2);
-
+if(length(GoodClusters)==0)
+    error('No good clusters.')
+end
 for unit = 1:length(GoodClusters)
     TSECS{unit+1} = spiketimes(clusternumbers==GoodClusters(unit))/30000;
     Units{unit+1} = GoodClusters(unit);
     
     % Finds position (a pair (x,y) in microns relative to the whole shank)
     % of the channel with the best waveform which was calculated in WaveformKK
-    [avgwaveform(unit+1),maxpeak(unit+1),channel(unit+1),width(unit+1)] = WaveformKK(allwaveforms(:,:, clusternumbers==GoodClusters(unit)));
-    position(unit+1) = {double(h5readatt(FilesKK.KWIK, ['/channel_groups/0/channels/',num2str(channel(unit+1)-1)],'position'))};
+    [avgwaveform(unit+1),maxpeak(unit+1),channel(unit+1),spikewidth(unit+1)] = WaveformKK(allwaveforms(:,:, clusternumbers==GoodClusters(unit)));
+    position(unit+1) = {double(h5readatt(FilesKK.KWIK, ['/channel_groups/',probe,'/channels/',num2str(channel(unit+1)-1)],'position'))};
     spikeoccurences(unit+1)=size(allwaveforms(:,:, clusternumbers==GoodClusters(unit)),3);
 end
-scatter(width(2:end),spikeoccurences(2:end))
+scatter(spikewidth(2:end),spikeoccurences(2:end))
 xlabel('width')
 ylabel('number of spikes')
 tsec = TSECS(:);
@@ -42,7 +55,7 @@ UnitID.units{1} = 0;
 UnitID.Wave.AverageWaveform=avgwaveform;
 UnitID.Wave.MaximumPeak=maxpeak;
 UnitID.Wave.Channel=channel;
-UnitID.Wave.Width=width;
+UnitID.Wave.Width=spikewidth;
 UnitID.Wave.Position=position;
 
 
