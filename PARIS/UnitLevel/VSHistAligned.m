4ea8a35388a6c88f385d85781950f82c29776ea7
Eric Song <bobobocuz@gmail.com>
2014-12-04 18:01:47 -0500

    Fixed extra argument error

--- PARIS/UnitLevel/VSHistAligned.m
+++ PARIS/UnitLevel/VSHistAligned.m
@@ -9,8 +9,8 @@ for Unit = 1:size(SpikeTimes.tsec,1)
     for Valve = 1:size(ValveTimes.PREXTimes,2)
         [CEM,~,~] = CrossExamineMatrix(ValveTimes.PREXTimes{Valve},st','hist');
         RasterAlign{Valve,Unit} = num2cell(CEM,2);
-        HistAlign = histc(CEM,Edges,2);
-        [HistAligned{Valve,Unit},~] = HistAlign/BinSize;
+        [HistAlign,~] = histc(CEM,Edges,2);
+        HistAligned{Valve,Unit} = HistAlign/BinSize;
         HistAlignSumRate{Valve,Unit} = sum(HistAlign)/(BinSize*size(ValveTimes.PREXTimes{Valve},2));
         HistAlignSmoothRate{Valve,Unit} = smooth(HistAlignSumRate{Valve,Unit},4);
     end
