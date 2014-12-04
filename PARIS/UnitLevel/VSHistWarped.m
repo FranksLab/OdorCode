239986873776e74019d0e9269a314a7b09e39da4
Eric Song <bobobocuz@gmail.com>
2014-12-04 18:02:31 -0500

    Finds the YLim for the plot of a range of valves/units(/trials)

--- PARIS/UnitLevel/VSHistWarped.m
+++ PARIS/UnitLevel/VSHistWarped.m
@@ -9,8 +9,8 @@ for Unit = 1:size(SpikeTimes.stwarped,1)
     for Valve = 1:size(ValveTimes.PREXTimeWarp,2)    
         [CEM,~,~] = CrossExamineMatrix(ValveTimes.PREXTimeWarp{Valve},st','hist');
         RasterWarp{Valve,Unit} = num2cell(CEM,2);
-        HistWarp = histc(CEM,Edges,2);
-        [HistWarped{Valve,Unit},~] = HistWarp/BinSize;
+        [HistWarp,~] = histc(CEM,Edges,2);
+        [HistWarped{Valve,Unit}] = HistWarp/BinSize;
         HistWarpSumRate{Valve,Unit} = sum(HistWarp)/(BinSize*size(ValveTimes.PREXTimes{Valve},2));
         HistWarpSmoothRate{Valve,Unit} = smooth(HistWarpSumRate{Valve,Unit},4);
     end
