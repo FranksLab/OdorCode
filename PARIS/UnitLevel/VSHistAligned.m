b7f67842603c5cd05a6c74ecebd71d2597cb30ee
Kevin Bolding <bolding@neuro.duke.edu>
2015-03-02 14:31:53 -0500

    Edited RasterXXX so that it only collects spikes between 5 pre and 10s post odor so the variable doesn't get so huge.

--- PARIS/UnitLevel/VSHistAligned.m
+++ PARIS/UnitLevel/VSHistAligned.m
@@ -9,9 +9,9 @@ for Unit = 1:size(SpikeTimes.tsec,1)
     for Valve = 1:size(ValveTimes.PREXTimes,2)
         [CEM,~,~] = CrossExamineMatrix(ValveTimes.PREXTimes{Valve},st','hist');
         RasterAlign{Valve,Unit} = num2cell(CEM,2);
-%         for k = 1:size(RasterAlign{Valve,Unit})
-%             RA(k).Times = RasterAlign{Valve,Unit}{k}(RasterAlign{Valve,Unit}{k}>-min(Edges) & RasterAlign{Valve,Unit}{k} < max(Edges));
-%         end
+        for k = 1:size(RasterAlign{Valve,Unit},1)
+            RasterAlign{Valve,Unit}{k} = RasterAlign{Valve,Unit}{k}(RasterAlign{Valve,Unit}{k}>-5 & RasterAlign{Valve,Unit}{k} < 10);
+        end
         
         HistAlign = histc(CEM,Edges,2);
         HistAligned{Valve,Unit} = HistAlign/BinSize;
