14b0861c728e9c8d9610666cac321f46ed79f302
Kevin Bolding <bolding@neuro.duke.edu>
2015-05-18 10:35:18 -0400

    Removed automatic creating of  "warped" (respirations all the same length) representations since we never use them.

--- PARIS/UnitLevel/GatherInfo1/PREXAssigner.m
+++ PARIS/UnitLevel/GatherInfo1/PREXAssigner.m
@@ -1,6 +1,6 @@
-function [PREXIndex,PREXTimes,PREXTimeWarp,FVTimeWarp] = PREXAssigner (FVSwitchTimesOn,PREX,tWarpLinear,Fs)
+function [PREXIndex,PREXTimes,PREXTimeWarp,FVTimeWarp] = PREXAssigner (FVSwitchTimesOn,PREX,Fs)
 
-PREXTimeWarp = cell(size(FVSwitchTimesOn));
+% PREXTimeWarp = cell(size(FVSwitchTimesOn));
 PREXTimes = cell(size(FVSwitchTimesOn));
 PREXIndex = cell(size(FVSwitchTimesOn));
 
@@ -9,8 +9,8 @@ for i = 1:length(FVSwitchTimesOn)
         [~,PREXTimes{i},PREXIndex{i}] = CrossExamineMatrix(FVSwitchTimesOn{i},PREX,'next');        
     end
 PREXIndex{i} = PREXIndex{i}';    
-PREXTimeWarp{i}  = tWarpLinear(round(PREXTimes{i}.*Fs));
-FVTimeWarp{i} = tWarpLinear(round(FVSwitchTimesOn{i}.*Fs));
+% PREXTimeWarp{i}  = tWarpLinear(round(PREXTimes{i}.*Fs));
+% FVTimeWarp{i} = tWarpLinear(round(FVSwitchTimesOn{i}.*Fs));
 end
 
 end
