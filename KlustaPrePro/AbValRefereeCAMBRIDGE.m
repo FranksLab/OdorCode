e6ae0d7d0e7430be900c680e8c312c73eb1cf5cd
Kevin Bolding <bolding@neuro.duke.edu>
2015-06-11 13:30:07 -0400

    Getting ready to process files from cambridge probes

---
+++ KlustaPrePro/AbValRefereeCAMBRIDGE.m
@@ -0,0 +1,73 @@
+function [hldr,badchan] = AbValRefereeCAMBRIDGE(data,ChannelCount,Bloc)
+%% make a data matrix
+ data = reshape(data,[ChannelCount length(data)/ChannelCount]); % to get back to linearized just use (:) on a matrix this shape
+ data = double(data);
+if ~isempty(Bloc)
+    dataS{1} = data(1:Bloc-1,:);
+    dataS{2} = data(Bloc:end,:);
+
+else
+    dataS{1} = data;
+end
+
+% Because we recorded all 64 channels even though there are only 16 per
+% bank. Let's keep only the channls with signals. Here's a list:
+signals = [3:8,13:14,20,23:29];
+
+for count=1:length(dataS)
+    
+    data = dataS{count};
+    data = data(signals,:);
+    
+    %% cut line noise
+    [B,A] = butter(2,[59/15000 61/15000],'stop');
+    dataline = filtfilt(B,A,data');
+    dataline = dataline';
+    
+    %% high pass filter the data
+    low = 100;
+    Fs = 30000;
+    [D,C] = butter(2,low/(Fs/2),'high');
+    data = filtfilt(D,C,dataline');
+    data = data';
+    
+    
+    
+    %% Exclude weird or dead channels from consideration as references.
+    % channels whose variance is different from the average variance by 1 SD
+    % are bad
+    a = std(data');
+    a = a';
+    b = abs((a-mean(a))/std(a));
+    
+    Excludos = b>2.5;
+    
+    %% Find the absolute value of the data.
+    abdata = abs(data);
+    abdata(Excludos,:) = NaN;  % get rid of bad channels
+    
+    %% now sort the magnitude of abdata matrix
+    [~,idx] = sort(abdata);
+    
+    %% average some number (~20%) of channels with low absolute values
+    chavnumb = floor(.2*size(data,1));
+    cnidx = sub2ind(size(abdata),idx(1:chavnumb,:),repmat(1:size(abdata,2),chavnumb,1));
+    refch = mean(data(cnidx),1);
+    
+    %% now use the raw data for subtraction.
+    % and subtract it from the original data
+    datarefd = bsxfun(@minus,data,refch);
+    
+    %% writing a headerless dat file for klustakwik
+%     if count==1
+%         hldr1 = int16(datarefd(:));
+%     else
+%         hldr2 = int16(datarefd(:));
+%     end
+    hldr{count} = int16(datarefd(:));
+    
+    badchan{count} = Excludos;
+end
+
+
+end
---
+++ KlustaPrePro/NS6ConcatCAMBRIDGE.m
@@ -0,0 +1,112 @@
+% clear all
+% close all
+% clc
+
+%% Hey, Kevin. You probably have to concatenate the NS3 file as well so you
+% can match experimental events with these spikes. That means you have to
+% think about the header.
+
+
+%% Select all files to concatenate
+[fname, path] = uigetfile('*.ns*', 'Choose an NSx file...','MultiSelect','on');
+if ~iscell(fname)
+    fname = {fname};
+end
+
+% fname is a cell containing all filenames to concatenate.
+% make chunks for each file separately
+for k = 1:length(fname)
+    FID = fopen([path fname{k}], 'r', 'ieee-le'); % open a file
+    dataHeaderBytes = 9; % headerbytes number from NPMK open* scripts
+    fseek(FID, 8, 'bof'); % move past filetype specifying bits
+    BasicHeader   = fread(FID, 306, '*uint8'); % read in the basic header to get HeaderBytes and ChannelCount
+    ChannelCount = double(typecast(BasicHeader(303:306), 'uint32')); % pull ChannelCount out of the header
+    %% find the connector bank letters to determine which channel
+    %  switches to a new probe (if the data is from multiple probes)
+    readSize= double(ChannelCount * 66);
+    ExtendedHeader = fread(FID, readSize, '*uint8'); % connector bank info is in here
+    for headerIDX = 1:ChannelCount %gathering all the A's and B's
+        offset = double((headerIDX-1)*66);
+        BankNum(headerIDX)=char(ExtendedHeader(21+offset) + ('A' - 1));
+    end
+    Bloc=find(BankNum=='B');
+    if numel(Bloc)>0 Bloc=Bloc(1); end
+    
+    %%
+    HeaderBytes   = double(typecast(BasicHeader(3:6), 'uint32')) + dataHeaderBytes; % how many Bytes to skip before data
+    fseek(FID, 0, 'eof'); % move to the end of the file
+    DataBytes = double(ftell(FID))-HeaderBytes; % measure how long the data is IN BYTES
+    DataPoints = DataBytes/2;
+    
+    
+    % work in 5 minute chunks, this should be set to the max multiple that Memory can handle
+    ChunkSize = 32*600*30000;
+    
+    % create a chunks folder if it doesn't exist, in the
+    % UnitSortingAnalysis folder
+    if ~exist([path,'chunks'],'dir')
+        mkdir(path,'chunks');
+    end
+    
+    % Start from headerbytes+0 and read a chunk (eg 0:1). next time start from one
+    % chunk size later (1:2). last iteration read to end of file (eg 2:2.5)
+    for j = 0:floor(DataPoints/ChunkSize)
+        ChunkPosition = HeaderBytes + (j)*ChunkSize*2; % Here ChunkSize is
+        % multiplied by two because the unit of ChunkSize is "datapoints"
+        % each of which takes 2 bytes. But ChunkPosition needs to be in
+        % bytes to tell fseek where to move.
+        
+        fseek(FID,ChunkPosition, 'bof');
+        if j == floor(DataPoints/ChunkSize) % indicating last iteration
+            Chunk = fread(FID, inf, '*int16');
+        else
+            Chunk = fread(FID, ChunkSize, '*int16');
+        end
+        
+        % Do the filtering and referencing for this chunk.
+        [ChunkAVR, badchan] = AbValRefereeCAMBRIDGE(Chunk,ChannelCount,Bloc);
+        %         size(ChunkAVR1)
+        %         size(ChunkAVR2)
+        for bank = 1:length(ChunkAVR)
+            % Chunk naming convention .chunk1.1.1 .chunk1.1.2 (file,chunk,bank) 1.2.1 2.1.1 etc
+            newfname = [path 'chunks\' fname{k}(1:15) '.chunk' num2str(k) '.' num2str(j,'%02.0f') '.' num2str(bank)];
+            % Opening the output file for saving
+            FIDw = fopen(newfname, 'w+', 'ieee-le');
+            fwrite(FIDw, ChunkAVR{bank}, 'int16');
+            fclose(FIDw);
+        end
+    end
+    
+    fclose(FID);
+    ChunksPerFile(k) = j+1;
+end
+%%
+
+% Chunk naming convention .chunk1.1.1 .chunk1.1.2 (file,chunk,bank) 1.2.1 2.1.1 etc
+for bank = 1:length(ChunkAVR)
+    CatSeries = [];
+    for k = 1:length(fname)
+        D = dir([path,'chunks\*.chunk',num2str(k),'*',num2str(bank)]);
+        [~,order] = sort( {D.name} );
+        D = D(order);
+        CatList = {D.name};
+        for j = 1:ChunksPerFile(k)
+            CatSeries = [CatSeries CatList{j} ,'+'];
+        end
+    end
+    CatCmd{bank} = ['copy /b ' CatSeries(1:end-1) ,' ', fname{1}(1:12) [num2str(bank),'COM.dat']];
+end
+%%
+
+cd([path,'chunks']);% move to the chunks directory
+for bank = 1:length(CatCmd)
+system(CatCmd{bank});
+badchan{bank}
+end
+system('del *chunk*')
+cd c:;
+
+%%
+%   %% in cmd.exe run this:
+%   copy /b Z:\UnitSortingAnalysis\31-Jul-2014_Analysis\chunks\*.* z:\UnitSortingAnalysis\chunked.dat
+% system('copy /b Z:\UnitSortingAnalysis\31-Jul-2014_Analysis\chunks\*.* z:\UnitSortingAnalysis\chunked1.dat')
