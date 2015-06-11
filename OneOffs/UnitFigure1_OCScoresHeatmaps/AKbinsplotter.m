4d1207f76623c36e54fff8f8fb7d21e0b0cc3ac0
Kevin Bolding <bolding@neuro.duke.edu>
2015-06-11 13:30:42 -0400

    changed Sparseness calculation but it is actually the same calculation you dummy

--- OneOffs/UnitFigure1_OCScoresHeatmaps/AKbinsplotter.m
+++ OneOffs/UnitFigure1_OCScoresHeatmaps/AKbinsplotter.m
@@ -55,21 +55,33 @@ Scores.SMPSTH.Align = Scores.SMPSTH.Align(VOI,2:end,:);
 
 %% Miura's way
 SparseVar = abs(squeeze(Scores.ZScore(:,:,1,:)));
-SparseTop = 1-(((nansum(SparseVar).^2)./(nansum(SparseVar.^2)))./sum(~isnan(SparseVar)));
+Top1 = (nansum(bsxfun(@rdivide,SparseVar, sum(~isnan(SparseVar))))).^2;
+Top2 = nansum(bsxfun(@rdivide,SparseVar.^2,sum(~isnan(SparseVar))));
+SparseTop = 1-Top1./Top2;
+% SparseTop = 1-(((nansum(SparseVar).^2)./(nansum(SparseVar.^2)))./sum(~isnan(SparseVar)));
 SparseBtm = 1-(1/sum(~isnan(SparseVar)));
 Scores.mSparseL = squeeze(SparseTop./SparseBtm);
 SparseVar = permute(SparseVar,[2,1,3]);
 SparseTop = 1-(((nansum(SparseVar).^2)./(nansum(SparseVar.^2)))./sum(~isnan(SparseVar)));
+Top1 = (nansum(bsxfun(@rdivide,SparseVar, sum(~isnan(SparseVar))))).^2;
+Top2 = nansum(bsxfun(@rdivide,SparseVar.^2,sum(~isnan(SparseVar))));
+SparseTop = 1-Top1./Top2;
 SparseBtm = 1-(1/sum(~isnan(SparseVar)));
 Scores.mSparseP = squeeze(SparseTop./SparseBtm);
 
 %% Vinje's way
 SparseVar = (squeeze(Scores.RawRate(:,:,1,:)));
 SparseTop = 1-(((nansum(SparseVar).^2)./(nansum(SparseVar.^2)))./sum(~isnan(SparseVar)));
+Top1 = (nansum(bsxfun(@rdivide,SparseVar, sum(~isnan(SparseVar))))).^2;
+Top2 = nansum(bsxfun(@rdivide,SparseVar.^2,sum(~isnan(SparseVar))));
+SparseTop = 1-Top1./Top2;
 SparseBtm = 1-(1/sum(~isnan(SparseVar)));
 Scores.vSparseL = squeeze(SparseTop./SparseBtm);
 SparseVar = permute(SparseVar,[2,1,3]);
 SparseTop = 1-(((nansum(SparseVar).^2)./(nansum(SparseVar.^2)))./sum(~isnan(SparseVar)));
+Top1 = (nansum(bsxfun(@rdivide,SparseVar, sum(~isnan(SparseVar))))).^2;
+Top2 = nansum(bsxfun(@rdivide,SparseVar.^2,sum(~isnan(SparseVar))));
+SparseTop = 1-Top1./Top2;
 SparseBtm = 1-(1/sum(~isnan(SparseVar)));
 Scores.vSparseP = squeeze(SparseTop./SparseBtm);
 
@@ -119,7 +131,7 @@ omUK = cat(1,OMNI.auROC{:,2})>.5;
 omDK = cat(1,OMNI.auROC{:,2})<.5;
 omRK = cat(1,OMNI.AURp{:,2})<.05;
 %% number responding to each odorant
-close all
+% close all
 figure
 positions = [200 100 800 400];
 set(gcf,'Position',positions)
