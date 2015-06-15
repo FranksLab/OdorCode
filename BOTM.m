f72d0caa3402658b1dc86d2336f4f357b816de4f
Eric Song <bobobocuz@gmail.com>
2015-06-15 14:33:56 -0400

    mahalanobis fun

--- BOTM.m
+++ BOTM.m
@@ -8,11 +8,19 @@ LFPdata=double(LFPdata);
 LFPdata=reshape(LFPdata,32,[]);
 LFPdata=LFPdata';
 LFPdata=LFPdata(:,[1:8 10:22 24:32]);
+Fs = 30000;
+[BR,AR] = butter(3, [500/(Fs/2)], 'high');
+hpdata = filtfilt(BR,AR,LFPdata);
+LFPdata = hpdata;
+
 FilesKK.KWIK='Z:/SortedKWIK/RecordSet002tef_1.kwik';
 FilesKK.KWX='Z:/KWX/RecordSet002tef_1.kwx';
 UnitID=SpikeTimesKK(FilesKK);
 spiketimes=UnitID.Spiketimes;
 avgwaveform=UnitID.Wave.AverageWaveform;
+clusternumbers=UnitID.ClusterNumbers;
+allwaveforms=UnitID.AllWaveforms;
+allwaveforms=double(allwaveforms);
 %% doing stuff with data
 diffspk=diff(spiketimes);
 [sorteddiffspk, diffspkindex]=sort(diffspk,'descend');
@@ -40,7 +48,7 @@ invCO=inv(COmean);
 %create whitened templates for k neurons
 clear D
 clear Dbotm
-for k = 2:length(avgwaveform) 
+for k = 2:length(avgwaveform)
     linearizedwaveforms{k-1}=avgwaveform{k}(:);
     whitenedtemp{k-1}=invCO*linearizedwaveforms{k-1};
     whitenedtemp{k-1}=reshape(whitenedtemp{k-1},48,[]);
@@ -52,10 +60,32 @@ for k = 2:length(avgwaveform)
     prior = log(length(UnitID.tsec{k-1})/size(LFPdata,1));
     pp(k-1) = length(UnitID.tsec{k-1})/size(LFPdata,1);
     Dbotm(k-1,:)=D(k-1,:)-.5*energy+prior;
+    
 end
 
 %%
 thresh = log(1-sum(pp));
-
 [maxxies,Ufire] = max(Dbotm);
 Ufire(maxxies<thresh) = nan;
+
+for unit=1:length(UnitID.units)-1
+    %% mahalanobis matrix from all waveforms from clustering
+    a=allwaveforms(:,:,clusternumbers==UnitID.units{unit+1});
+    a=permute(a,[2 1 3]);
+    a=reshape(a,30*48, 1,[]);
+    a=squeeze(a);
+    
+    %% mahal matrix from template matching
+    b = [];
+    matchindex=find(Ufire==unit);
+    for k=1:length(matchindex)
+        chunk=LFPdata(matchindex(k):matchindex(k)+47,:);
+        b(:,k)=chunk(:);
+    end
+    
+    %% calculating mahal distance
+    fullmaha=pdist([mean(a,2)';b'],'seuclidean',nanstd(a'));
+    maha{unit}=fullmaha(1:size(b,2));
+    
+%     clear a; clear b;
+end
